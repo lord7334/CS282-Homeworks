fb8b0a0b461399576a230f727b95c169bf787696
FrankLikuohao <frank.likuohao@gmail.com>
2017-05-23 21:43:17 +0800

    QQ

--- ConsoleApplication1/Example01/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/Example01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Example01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("Example01")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("de7fa7b6-bc42-4597-be59-45b3de60572e")]
+[assembly: Guid("5966b832-401e-49d0-9255-870bf751c15d")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
