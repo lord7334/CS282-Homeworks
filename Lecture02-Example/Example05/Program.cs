b842e60264835371749c01e691b8f0a2ba5b60a2
lord7334 <lord7334@gmail.com>
2017-05-26 22:10:54 +0800

    未完成的更新

---
+++ Lecture02-Example/Example05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Resnick",
+                Birthday = "1223",
+                Email = "resnick1223@gmail.com",
+                Orientation = 0.0,
+                Pos = new Position()
+                {
+                    X = 10.0,
+                    Y = 20.0
+                }
+            };
+            Console.Write("請輸入要畫的方形的邊長:");
+            double width = 0.0;
+            bool success = double.TryParse(Console.ReadLine(), out width);
+            if (success)
+                person.DrawSquare(width);
+            else
+                Console.WriteLine("輸入錯誤，請重新執行!");
+        }
+    }
+}
---
+++ Lecture02-Example/Example06/Praducts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example06
+{
+    internal class Praducts
+    {
+    }
+}
