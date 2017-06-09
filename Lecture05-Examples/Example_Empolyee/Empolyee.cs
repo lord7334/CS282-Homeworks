b1575d2226a66ab23107859a167935f58d2d63f9
lord7334 <lord7334@gmail.com>
2017-06-09 22:19:26 +0800

    更新進度

---
+++ Lecture05-Examples/Example_Empolyee/Empolyee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_Empolyee
+{
+    public class Empolyee
+    {
+        private double basesalary;
+        private double benefit;
+        public double salary;
+
+        public double BaseSalary
+        {
+            get
+            {
+                return this.basesalary;
+            }
+
+            set
+            {
+                if (value > 50000)
+                {
+                    this.basesalary = 50000;
+                }
+                else if (value < 21009)
+                {
+                    this.basesalary = 21009;
+                }
+                else
+                {
+                    this.basesalary = value;
+                }
+            }
+        }
+
+        public double Humidity
+        {
+            get => benefit;
+            set => benefit = value;
+        }
+
+        public void SetEmpolyee(double value)
+        {
+            if (value > 50000)
+            {
+                this.basesalary = 50000;
+            }
+            else if (value < 21009)
+            {
+                this.basesalary = 21009;
+            }
+            else
+            {
+                this.basesalary = value;
+            }
+        }
+
+        public double GetEmpolyee()
+        {
+            return this.basesalary;
+        }
+
+        public bool Equals(Empolyee other)
+        {
+            return this.BaseSalary == other.BaseSalary
+                   && this.Humidity == other.Humidity
+                   && this.salary == other.salary;
+        }
+    }
+}
---
+++ Lecture05-Examples/Example_Empolyee/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_Empolyee
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Empolyee data = new Empolyee();
+            Console.Write("請輸入底薪及獎金:");
+            double temperature = double.Parse(Console.ReadLine());
+
+            data.GetEmpolyee = Empolyee;
+            Console.WriteLine("您輸入的是{0}, 結果是{1}", temperature, data.Temperature);
+        }
+    }
+}
